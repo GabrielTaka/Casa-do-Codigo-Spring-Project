61da1ef90666a1fdcb752ab449d87670c43379e5
Gabriel <gabriel.vargas01@fatec.sp.gov.br>
2019-08-05 04:42:23 -0300

    configuranção de banco de dados

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,12 +1,17 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.Produtodao;
 import br.com.casadocodigo.loja.models.Produto;
 
 @Controller
 public class ProdutosController {
+	
+	@Autowired
+	private Produtodao produtodao;
 	
 	@RequestMapping("/produtos/form")
 	public String form() {
@@ -16,6 +21,7 @@ public class ProdutosController {
 	@RequestMapping("/produtos")
 	public String grava(Produto produto) {
 		System.out.println( produto );
-		return "ok";
+		produtodao.gravar(produto);
+		return "produtos/Ok";
 	}
 }
--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,6 +1,19 @@
 package br.com.casadocodigo.loja.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.boot.registry.selector.StrategyRegistration;
+
+@Entity
 public class Produto {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private int id;
+	
 	private String titulo; 
 	private String descricao;
 	private int paginas;
